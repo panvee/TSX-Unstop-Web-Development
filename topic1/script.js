652ea89f40660db6b72703cba961b39bbdb31c46
Naidu Panvee <panvee_naidu@srmap.edu.in>
2025-05-14 22:22:37 +0530

    Added Topic 1 - Core Trio project

--- topic1/script.js
+++ topic1/script.js
@@ -1,1 +1,8 @@
+// Log a message when the page loads
 console.log("Page has loaded successfully!");
+
+// Add button click functionality
+document.getElementById('clickButton').addEventListener('click', function() {
+    console.log("Button was clicked!");
+    alert("Hello from JavaScript!");
+});
